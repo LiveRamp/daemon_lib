a1f69e3731545540f10f858651de873456b73faa
Armaan Sarkar <asarkar@liveramp.com>
2015-05-15 16:57:33 -0700

    move bin script into a resource

--- src/java/com/liveramp/daemon_lib/utils/ForkedJobletRunner.java
+++ src/java/com/liveramp/daemon_lib/utils/ForkedJobletRunner.java
@@ -1,6 +1,12 @@
 package com.liveramp.daemon_lib.utils;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+
+import com.google.common.io.ByteStreams;
+import org.apache.commons.io.FileUtils;
 
 import com.liveramp.daemon_lib.Joblet;
 import com.liveramp.daemon_lib.JobletConfig;
@@ -9,22 +15,36 @@ import com.liveramp.daemon_lib.processes.ProcessUtil;
 import com.liveramp.java_support.logging.LoggingHelper;
 
 public class ForkedJobletRunner {
-  private static final String JOBLET_RUNNER_SCRIPT = "src/java/com/liveramp/daemon_lib/utils/joblet_runner.sh";
+  private static final String JOBLET_RUNNER_SCRIPT = "bin/joblet_runner.sh";
+  private static final String JOBLET_RUNNER_SCRIPT_SOURCE = "com/liveramp/daemon_lib/utils/joblet_runner.txt";
 
   public static ForkedJobletRunner production() {
     return new ForkedJobletRunner();
   }
 
   public int run(Class<? extends JobletFactory<? extends JobletConfig>> jobletFactoryClass, JobletConfigStorage configStore, String cofigIdentifier) throws IOException {
+    prepareScript();
     // TODO(asarkar): find a better way to escape the class name
     int pid = ProcessUtil.runCommand(JOBLET_RUNNER_SCRIPT, "\'" + jobletFactoryClass.getName() + "\'", configStore.getPath(), cofigIdentifier);
 
     return pid;
   }
 
+  private static void prepareScript() throws IOException {
+    File productionScript = new File(JOBLET_RUNNER_SCRIPT);
+    if (!productionScript.exists()) {
+      InputStream scriptResourceInput = ForkedJobletRunner.class.getClassLoader().getResourceAsStream(JOBLET_RUNNER_SCRIPT_SOURCE);
+
+      FileUtils.forceMkdir(productionScript.getParentFile());
+      FileOutputStream scriptProductionOutput = new FileOutputStream(JOBLET_RUNNER_SCRIPT);
+
+      ByteStreams.copy(scriptResourceInput, scriptProductionOutput);
+    }
+  }
+
   public static void main(String[] args) throws IOException, ClassNotFoundException, IllegalAccessException, InstantiationException {
     LoggingHelper.setLoggingProperties("forked-joblet-runner");
-
+    
     String jobletFactoryClassName = args[0];
     String configStorePath = args[1];
     String id = args[2];
