1c762662820dc9ccb568c9bff5a41100703db25d
Armaan Sarkar <asarkar@liveramp.com>
2015-05-18 16:34:14 -0700

    add appropriate generic type information

--- src/java/com/liveramp/daemon_lib/executors/BlockingJobletExecutor.java
+++ src/java/com/liveramp/daemon_lib/executors/BlockingJobletExecutor.java
@@ -5,15 +5,15 @@ import com.liveramp.daemon_lib.JobletConfig;
 import com.liveramp.daemon_lib.JobletFactory;
 import com.liveramp.daemon_lib.utils.DaemonException;
 
-public class BlockingJobletExecutor implements JobletExecutor {
-  private final JobletFactory jobletFactory;
+public class BlockingJobletExecutor<T extends JobletConfig> implements JobletExecutor<T> {
+  private final JobletFactory<T> jobletFactory;
 
-  public BlockingJobletExecutor(JobletFactory jobletFactory) {
+  public BlockingJobletExecutor(JobletFactory<T> jobletFactory) {
     this.jobletFactory = jobletFactory;
   }
 
   @Override
-  public void execute(JobletConfig jobletConfig) throws DaemonException {
+  public void execute(T jobletConfig) throws DaemonException {
     Joblet joblet = jobletFactory.create(jobletConfig);
     joblet.run();
   }
