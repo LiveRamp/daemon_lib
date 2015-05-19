bce43c0f8ae0eb25a0bdd5710fece56af4c077bb
Armaan Sarkar <asarkar@liveramp.com>
2015-05-19 11:46:55 -0700

    add exception to method signature

--- src/java/com/liveramp/daemon_lib/JobletFactory.java
+++ src/java/com/liveramp/daemon_lib/JobletFactory.java
@@ -1,5 +1,7 @@
 package com.liveramp.daemon_lib;
 
+import com.liveramp.daemon_lib.utils.DaemonException;
+
 public interface JobletFactory<T extends JobletConfig> {
-  public abstract Joblet create(T config);
+  public abstract Joblet create(T config) throws DaemonException;
 }
