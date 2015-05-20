7e4f56ff8e571be70e6c26ceb7c59b52dc1d3f56
Armaan Sarkar <asarkar@liveramp.com>
2015-05-19 17:38:02 -0700

    better alerts handling

--- src/java/com/liveramp/daemon_lib/Daemon.java
+++ src/java/com/liveramp/daemon_lib/Daemon.java
@@ -7,24 +7,29 @@ import org.slf4j.LoggerFactory;
 
 import com.liveramp.daemon_lib.executors.JobletExecutor;
 import com.liveramp.daemon_lib.utils.DaemonException;
+import com.liveramp.java_support.alerts_handler.AlertsHandler;
+import com.liveramp.java_support.alerts_handler.recipients.AlertRecipients;
+import com.liveramp.java_support.alerts_handler.recipients.AlertSeverity;
 
 public class Daemon<T extends JobletConfig> {
   private static final Logger LOG = LoggerFactory.getLogger(Daemon.class);
 
   private final String identifier;
   private final JobletExecutor<T> executor;
+  private final AlertsHandler alertsHandler;
   private final JobletConfigProducer<T> configProducer;
 
   private boolean running;
 
-  public Daemon(String identifier, JobletExecutor<T> executor, JobletConfigProducer<T> configProducer) {
+  public Daemon(String identifier, JobletExecutor<T> executor, JobletConfigProducer<T> configProducer, AlertsHandler alertsHandler) {
     this.identifier = identifier;
     this.configProducer = configProducer;
     this.executor = executor;
+    this.alertsHandler = alertsHandler;
     this.running = false;
   }
 
-  public final void start() throws DaemonException {
+  public final void start() {
     LOG.info("Starting daemon ({})", identifier);
     running = true;
 
@@ -36,17 +41,26 @@ public class Daemon<T extends JobletConfig> {
     LOG.info("Exiting daemon ({})", identifier);
   }
 
-  protected boolean processNext() throws DaemonException {
+  protected boolean processNext() {
     if (executor.canExecuteAnother()) {
-      T jobletConfig = configProducer.getNextConfig();
+      T jobletConfig;
+      try {
+        jobletConfig = configProducer.getNextConfig();
+      } catch (DaemonException e) {
+        alertsHandler.sendAlert("Error getting next config for daemon (" + identifier + ")", e, AlertRecipients.engineering(AlertSeverity.ERROR));
+        return false;
+      }
 
       if (jobletConfig != null) {
         LOG.info("Found joblet config: " + jobletConfig);
-        executor.execute(jobletConfig);
+        try {
+          executor.execute(jobletConfig);
+        } catch (Exception e) {
+          alertsHandler.sendAlert("Error executing joblet config for daemon (" + identifier + ")", jobletConfig.toString(), e, AlertRecipients.engineering(AlertSeverity.ERROR));
+        }
         return true;
       }
     }
-
     return false;
   }
 
