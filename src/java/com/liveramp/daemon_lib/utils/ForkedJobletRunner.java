fdf6f6bb320b27c0ca55e30c32f3840babca2182
Armaan Sarkar <asarkar@liveramp.com>
2015-05-26 15:42:19 -0700

    Revert "apparently commandline args are automatically unquoted - or so i think"
    This reverts commit d86e99a2e2e435b30c291d57033714da57e26b6b.

--- src/java/com/liveramp/daemon_lib/utils/ForkedJobletRunner.java
+++ src/java/com/liveramp/daemon_lib/utils/ForkedJobletRunner.java
@@ -51,10 +51,18 @@ public class ForkedJobletRunner {
     return "'" + s + "'";
   }
 
+  private static String unquote(String s) {
+    if (s.charAt(0) == '\'' && s.charAt(s.length() - 1) == '\'') {
+      return s.substring(1, s.length() - 1);
+    } else {
+      throw new IllegalArgumentException("String is not quoted");
+    }
+  }
+
   public static void main(String[] args) throws IOException, ClassNotFoundException, IllegalAccessException, InstantiationException, DaemonException {
     LoggingHelper.setLoggingProperties("forked-joblet-runner");
 
-    String jobletFactoryClassName = args[0];
+    String jobletFactoryClassName = unquote(args[0]);
     String configStorePath = args[1];
     String id = args[2];
 
