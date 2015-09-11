5688020a964ff71ff8dcc63f37f8ee6b89e2db38
Michel Tricot <michel@liveramp.com>
2015-09-11 13:40:28 -0700

    use new iface

--- src/java/com/liveramp/daemon_lib/utils/DaemonRunner.java
+++ src/java/com/liveramp/daemon_lib/utils/DaemonRunner.java
@@ -5,19 +5,18 @@ import com.liveramp.liveramp_hlib.util.SocketEnsureOneProcessInstance;
 
 public class DaemonRunner {
   public static void run(Daemon daemon) throws Exception {
-    new SingleDaemonProcess(daemon).ensure();
+    new SocketEnsureOneProcessInstance(daemon.getIdentifier()).ensure(new SingleDaemonProcess(daemon));
   }
 
-  private static class SingleDaemonProcess extends SocketEnsureOneProcessInstance {
+  private static class SingleDaemonProcess implements Runnable {
     private final Daemon daemon;
 
     public SingleDaemonProcess(Daemon daemon) {
-      super(daemon.getIdentifier());
       this.daemon = daemon;
     }
 
     @Override
-    protected void run() throws Exception {
+    public void run() {
       Runtime.getRuntime().addShutdownHook(new ShutdownHandler(daemon));
       daemon.start();
     }
