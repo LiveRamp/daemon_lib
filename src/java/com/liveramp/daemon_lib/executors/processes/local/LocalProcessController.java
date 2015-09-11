88f80a90ef122743d974be82bcf135fb05eded74
Michel Tricot <michel@liveramp.com>
2015-09-11 16:58:51 -0700

    add logging

--- src/java/com/liveramp/daemon_lib/executors/processes/local/LocalProcessController.java
+++ src/java/com/liveramp/daemon_lib/executors/processes/local/LocalProcessController.java
@@ -76,6 +76,7 @@ public class LocalProcessController<T extends ProcessMetadata> implements Proces
     public void run() {
       try {
         List<ProcessDefinition<T>> watchedProcesses = getWatchedProcesses(fsHelper, true);
+        LOG.info("Watched Processes {}", watchedProcesses);
         Map<Integer, PidGetter.PidData> runningPids = pidGetter.getPids();
         Iterator<ProcessDefinition<T>> iterator = watchedProcesses.iterator();
         while (iterator.hasNext()) {
