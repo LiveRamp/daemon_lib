f7a6bb3ea507b70a7961b96a21f51319b14c6e5f
Michel Tricot <michel@liveramp.com>
2015-09-11 11:37:32 -0700

    Rename class

--- src/java/com/liveramp/daemon_lib/utils/DaemonRunner.java
+++ src/java/com/liveramp/daemon_lib/utils/DaemonRunner.java
@@ -1,14 +1,14 @@
 package com.liveramp.daemon_lib.utils;
 
 import com.liveramp.daemon_lib.Daemon;
-import com.liveramp.liveramp_hlib.util.EnsureOneProcessInstance;
+import com.liveramp.liveramp_hlib.util.SocketEnsureOneProcessInstance;
 
 public class DaemonRunner {
   public static void run(Daemon daemon) throws Exception {
     new SingleDaemonProcess(daemon).ensure();
   }
 
-  private static class SingleDaemonProcess extends EnsureOneProcessInstance {
+  private static class SingleDaemonProcess extends SocketEnsureOneProcessInstance {
     private final Daemon daemon;
 
     public SingleDaemonProcess(Daemon daemon) {
