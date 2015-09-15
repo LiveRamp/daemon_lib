2b4dfd00c90eea8c7b3e71e5c7458e9c10c6ce2c
Michel Tricot <michel@liveramp.com>
2015-09-14 17:47:55 -0700

    add empty config

---
+++ src/java/com/liveramp/daemon_lib/built_in/EmptyConfig.java
@@ -0,0 +1,10 @@
+package com.liveramp.daemon_lib.built_in;
+
+import com.liveramp.daemon_lib.JobletConfig;
+
+public class EmptyConfig implements JobletConfig {
+  @Override
+  public String toString() {
+    return "EmptyConfig{}";
+  }
+}
