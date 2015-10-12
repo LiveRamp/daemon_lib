91b8a1d0f8a7636b90259b1d54b67b27c13ac238
Armaan Sarkar <asarkar@liveramp.com>
2015-10-12 01:59:29 -0700

    Revert "add logging"
    This reverts commit 45a519c159ffa2a78f158b90ddd2193f52bf0b53.

--- src/java/com/liveramp/daemon_lib/utils/JobletProcessHandler.java
+++ src/java/com/liveramp/daemon_lib/utils/JobletProcessHandler.java
@@ -1,7 +1,5 @@
 package com.liveramp.daemon_lib.utils;
 
-import org.slf4j.Logger;
-
 import com.liveramp.daemon_lib.JobletCallback;
 import com.liveramp.daemon_lib.JobletConfig;
 import com.liveramp.daemon_lib.executors.processes.ProcessDefinition;
@@ -9,11 +7,7 @@ import com.liveramp.daemon_lib.executors.processes.local.ProcessHandler;
 import com.liveramp.daemon_lib.tracking.JobletStatus;
 import com.liveramp.daemon_lib.tracking.JobletStatusManager;
 
-import static org.slf4j.LoggerFactory.getLogger;
-
 public class JobletProcessHandler<T extends JobletConfig> implements ProcessHandler<JobletConfigMetadata> {
-  private static final Logger LOG = getLogger(JobletProcessHandler.class);
-
   private final JobletCallback<T> successCallback;
   private final JobletCallback<T> failureCallback;
   private final JobletConfigStorage<T> configStorage;
@@ -33,11 +27,9 @@ public class JobletProcessHandler<T extends JobletConfig> implements ProcessHand
     try {
       JobletConfigMetadata metadata = watchedProcess.getMetadata();
       T jobletConfig = configStorage.loadConfig(metadata.getIdentifier());
-      LOG.info("Calling onRemove callback for config {}", jobletConfig);
       postExecutionCallback.callback(jobletConfig);
       if (jobletStatusManager.exists(metadata.getIdentifier())) {
         JobletStatus status = jobletStatusManager.getStatus(metadata.getIdentifier());
-        LOG.info("Joblet status: {}", status);
         switch (status) {
           case DONE:
             successCallback.callback(jobletConfig);
