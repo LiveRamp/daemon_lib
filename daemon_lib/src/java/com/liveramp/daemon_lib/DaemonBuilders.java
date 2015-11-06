3b881e3eace89b49f54282c37ec921e701a0fbb7
Roshan George <rgeorge@liveramp.com>
2015-11-05 18:21:31 -0800

    Deprecate ForkingDaemonBuilder that uses JobletCallbacks

--- daemon_lib/src/java/com/liveramp/daemon_lib/DaemonBuilders.java
+++ daemon_lib/src/java/com/liveramp/daemon_lib/DaemonBuilders.java
@@ -8,6 +8,14 @@ import com.liveramp.daemon_lib.builders.ThreadingDaemonBuilder;
 import com.liveramp.java_support.alerts_handler.AlertsHandler;
 
 public class DaemonBuilders {
+  /**
+   * @deprecated Use {@link this#forked(String, String, Class, JobletConfigProducer, AlertsHandler)} and then set
+   * callbacks using:
+   *  * {@link ForkingDaemonBuilder#setOnNewConfigCallback(JobletCallback)}
+   *  * {@link ForkingDaemonBuilder#setFailureCallback(JobletCallback)}
+   *  * {@link ForkingDaemonBuilder#setSuccessCallback(JobletCallback)}
+   */
+  @Deprecated
   public static <T extends JobletConfig> ForkingDaemonBuilder<T> forked(String workingDir, String identifier, Class<? extends JobletFactory<T>> jobletFactoryClass, JobletConfigProducer<T> jobletConfigProducer, AlertsHandler alertsHandler, JobletCallbacks<T> jobletCallbacks) throws IllegalAccessException, IOException, InstantiationException {
     return new ForkingDaemonBuilder<>(
         workingDir,
@@ -18,6 +26,18 @@ public class DaemonBuilders {
         alertsHandler
     );
   }
+
+  public static <T extends JobletConfig> ForkingDaemonBuilder<T> forked(String workingDir, String identifier, Class<? extends JobletFactory<T>> jobletFactoryClass, JobletConfigProducer<T> jobletConfigProducer, AlertsHandler alertsHandler) throws IllegalAccessException, IOException, InstantiationException {
+    return new ForkingDaemonBuilder<>(
+        workingDir,
+        identifier,
+        jobletFactoryClass,
+        jobletConfigProducer,
+        new JobletCallbacks.None<T>(),
+        alertsHandler
+    );
+  }
+
 
   public static <T extends JobletConfig> BlockingDaemonBuilder<T> blocking(String identifier, JobletFactory<T> jobletFactory, JobletConfigProducer<T> jobletConfigProducer, AlertsHandler alertsHandler, JobletCallbacks<T> jobletCallbacks) throws InstantiationException, IllegalAccessException {
     return new BlockingDaemonBuilder<>(
