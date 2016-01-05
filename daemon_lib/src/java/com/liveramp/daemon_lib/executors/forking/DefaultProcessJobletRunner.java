2938be1ddc5aa25229070249bd5319bfa4a930c0
Armaan Sarkar <asarkar@liveramp.com>
2016-01-04 16:35:16 -0800

    logging

--- daemon_lib/src/java/com/liveramp/daemon_lib/executors/forking/DefaultProcessJobletRunner.java
+++ daemon_lib/src/java/com/liveramp/daemon_lib/executors/forking/DefaultProcessJobletRunner.java
@@ -3,12 +3,18 @@ package com.liveramp.daemon_lib.executors.forking;
 import java.io.IOException;
 import java.util.Map;
 
+import org.slf4j.Logger;
+
 import com.liveramp.daemon_lib.JobletConfig;
 import com.liveramp.daemon_lib.JobletFactory;
 import com.liveramp.daemon_lib.executors.processes.ProcessUtil;
 import com.liveramp.daemon_lib.utils.JobletConfigStorage;
 
+import static org.slf4j.LoggerFactory.getLogger;
+
 public class DefaultProcessJobletRunner implements ProcessJobletRunner {
+  private static final Logger LOG = getLogger(DefaultProcessJobletRunner.class);
+
   @Override
   public int run(Class<? extends JobletFactory<? extends JobletConfig>> jobletFactoryClass, JobletConfigStorage configStore, String cofigIdentifier, Map<String, String> envVariables, String workingDir) throws IOException {
     String separator = System.getProperty("file.separator");
@@ -25,6 +31,9 @@ public class DefaultProcessJobletRunner implements ProcessJobletRunner {
             cofigIdentifier);
 
     processBuilder.environment().putAll(envVariables);
+
+    LOG.info("Running command: {}", processBuilder.command());
+
     int pid = ProcessUtil.run(processBuilder);
 
     return pid;
