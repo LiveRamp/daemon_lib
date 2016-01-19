24c5672ce8aa09fdb8496c90f2c2ca1e393c4d92
Andreea Georgescu <ageorgescu@liveramp.com>
2016-01-19 11:08:56 -0800

    Added DaemonBuilders methods without alertsHandler

--- daemon_lib/src/java/com/liveramp/daemon_lib/DaemonBuilders.java
+++ daemon_lib/src/java/com/liveramp/daemon_lib/DaemonBuilders.java
@@ -35,4 +35,28 @@ public class DaemonBuilders {
         alertsHandler
     );
   }
+
+  public static <T extends JobletConfig> ForkingDaemonBuilder<T> forked(String workingDir, String identifier, Class<? extends JobletFactory<T>> jobletFactoryClass, JobletConfigProducer<T> jobletConfigProducer) throws IllegalAccessException, IOException, InstantiationException {
+    return new ForkingDaemonBuilder<>(
+        workingDir,
+        identifier,
+        jobletFactoryClass,
+        jobletConfigProducer
+    );
+  }
+
+  public static <T extends JobletConfig> BlockingDaemonBuilder<T> blocking(String identifier, JobletFactory<T> jobletFactory, JobletConfigProducer<T> jobletConfigProducer) throws InstantiationException, IllegalAccessException {
+    return new BlockingDaemonBuilder<>(
+        identifier,
+        jobletFactory,
+        jobletConfigProducer);
+  }
+
+  public static <T extends JobletConfig> ThreadingDaemonBuilder<T> threaded(String identifier, JobletFactory<T> jobletFactory, JobletConfigProducer<T> jobletConfigProducer) throws IllegalAccessException, IOException, InstantiationException {
+    return new ThreadingDaemonBuilder<>(
+        identifier,
+        jobletFactory,
+        jobletConfigProducer
+    );
+  }
 }
