0bbc218000b6040e7ae38a6559f9c91f9e8db0aa
Romain Faugeroux <rfaugeroux@liveramp.com>
2016-12-07 10:58:44 -0800

    Logging daemon notifier (#81)
    * add logging daemon notifier
    
    * decorate notifier with logging
    
    * nl

---
+++ src/java/com/liveramp/daemon_lib/utils/LoggingForwardingNotifier.java
@@ -0,0 +1,31 @@
+package com.liveramp.daemon_lib.utils;
+
+import com.google.common.base.Optional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.liveramp.daemon_lib.DaemonNotifier;
+
+public class LoggingForwardingNotifier implements DaemonNotifier {
+
+  private static Logger LOG = LoggerFactory.getLogger(LoggingForwardingNotifier.class);
+
+  private final DaemonNotifier delegate;
+
+  public LoggingForwardingNotifier(DaemonNotifier delegate) {
+    this.delegate = delegate;
+  }
+
+  @Override
+  public void notify(String subject, Optional<String> body, Optional<? extends Throwable> t) {
+
+    String message = String.format("%s\n%s", subject, body.or(""));
+
+    if (t.isPresent()) {
+      LOG.error(message, t.get());
+    } else {
+      LOG.error(message);
+    }
+    delegate.notify(subject, body, t);
+  }
+}
