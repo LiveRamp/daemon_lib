140405b6c087c569a4535d25f65cdb8034e27dc7
Tenzing Shaw <tshaw@liveramp.com>
2016-12-06 17:06:03 -0800

    support providing jvm arguments for jar based joblet runner

--- src/java/com/liveramp/daemon_lib/executors/forking/JarBasedProcessJobletRunner.java
+++ src/java/com/liveramp/daemon_lib/executors/forking/JarBasedProcessJobletRunner.java
@@ -1,9 +1,13 @@
 package com.liveramp.daemon_lib.executors.forking;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
 import com.google.common.base.Joiner;
+import com.google.common.collect.ImmutableList;
 import org.slf4j.Logger;
 
 import com.liveramp.daemon_lib.JobletConfig;
@@ -18,29 +22,33 @@ public class JarBasedProcessJobletRunner implements ProcessJobletRunner<Integer>
 
   private final String executableCommand;
   private final String jarPath;
+  private final List<String> jvmOptions;
 
-  private JarBasedProcessJobletRunner(String executableCommand, String jarPath) {
+  private JarBasedProcessJobletRunner(String executableCommand, String jarPath, List<String> jvmOptions) {
     this.executableCommand = executableCommand;
     this.jarPath = jarPath;
+    this.jvmOptions = jvmOptions;
   }
 
   @Override
   public Integer run(Class<? extends JobletFactory<? extends JobletConfig>> jobletFactoryClass,
-                 JobletConfigStorage configStore,
-                 String cofigIdentifier,
-                 Map<String, String> envVariables,
-                 String workingDir) throws IOException, ClassNotFoundException {
-    ProcessBuilder processBuilder =
-        new ProcessBuilder(
-            executableCommand,
-            "-cp",
-            jarPath,
-            ForkedJobletRunner.class.getName(),
-            ForkedJobletRunner.quote(jobletFactoryClass.getName()),
-            configStore.getPath(),
-            workingDir,
-            cofigIdentifier
-        );
+                     JobletConfigStorage configStore,
+                     String cofigIdentifier,
+                     Map<String, String> envVariables,
+                     String workingDir) throws IOException, ClassNotFoundException {
+    ImmutableList.Builder<String> commandListBuilder = ImmutableList.builder();
+    commandListBuilder.add(executableCommand);
+    commandListBuilder.addAll(jvmOptions);
+    commandListBuilder.addAll(Arrays.asList(
+        "-cp",
+        jarPath,
+        ForkedJobletRunner.class.getName(),
+        ForkedJobletRunner.quote(jobletFactoryClass.getName()),
+        configStore.getPath(),
+        workingDir,
+        cofigIdentifier
+    ));
+    ProcessBuilder processBuilder = new ProcessBuilder(commandListBuilder.build());
 
     processBuilder.environment().putAll(envVariables);
 
@@ -54,26 +62,31 @@ public class JarBasedProcessJobletRunner implements ProcessJobletRunner<Integer>
 
     private String jarPath;
     private String executableCommand;
+    private List<String> jvmOptions;
 
     private Builder(String jarPath) {
       this.jarPath = jarPath;
       this.executableCommand = DEFAULT_COMMAND;
+      this.jvmOptions = new ArrayList<>();
     }
 
     public Builder setJarPath(String jarPath) {
       this.jarPath = jarPath;
-
       return this;
     }
 
     public Builder setExecutableCommand(String executableCommand) {
       this.executableCommand = executableCommand;
+      return this;
+    }
 
+    public Builder addJvmOptions(String... options) {
+      jvmOptions.addAll(Arrays.asList(options));
       return this;
     }
 
     public JarBasedProcessJobletRunner build() {
-      return new JarBasedProcessJobletRunner(executableCommand, jarPath);
+      return new JarBasedProcessJobletRunner(executableCommand, jarPath, jvmOptions);
     }
   }
 
