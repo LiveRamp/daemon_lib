e69a76a26c3426b53aba539d06cbdd7e3f7b06bb
Porter Westling <pwestling@liveramp.com>
2017-02-27 18:01:14 -0800

    Revert "Merge pull request #5 from LiveRamp/serverid_error_escape_hatch"
    This reverts commit ee5747c15774d49da64eea9745be701d0f6a3bd7, reversing
    changes made to acc0b7243143626924369cf19adbbb7316dbda48.

--- src/main/java/com/liveramp/daemon_lib/utils/JobletConfigStorage.java
+++ src/main/java/com/liveramp/daemon_lib/utils/JobletConfigStorage.java
@@ -13,7 +13,6 @@ import org.apache.commons.lang.SerializationUtils;
 import com.liveramp.daemon_lib.JobletConfig;
 
 public class JobletConfigStorage<T extends JobletConfig> {
-  public static final String JOBLET_CONFIG_SERIAL_VERSION_UID_RECOVERY_PROPERTY = "joblet.config.serverid.recover";
   private final String basePath;
 
   public JobletConfigStorage(String basePath) {
@@ -38,10 +37,7 @@ public class JobletConfigStorage<T extends JobletConfig> {
 
   public T loadConfig(String identifier) throws IOException, ClassNotFoundException {
     try {
-      FileInputStream in = new FileInputStream(getPath(identifier));
-      ObjectInputStream ois = System.getProperty(JOBLET_CONFIG_SERIAL_VERSION_UID_RECOVERY_PROPERTY) == null ?
-          new ObjectInputStream(in) :
-          new JobletConfigRecoveryStream(in);
+      ObjectInputStream ois = new ObjectInputStream(new FileInputStream(getPath(identifier)));
       T config = (T)ois.readObject();
       ois.close();
 
