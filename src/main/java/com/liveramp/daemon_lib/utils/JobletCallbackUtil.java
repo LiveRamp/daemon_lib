a41162a86bcce2daaf3a4933fcf3254c0e19db2c
Liren Tu <ltu@liveramp.com>
2017-07-27 15:13:50 -0700

    Modify joblet callback util

--- src/main/java/com/liveramp/daemon_lib/utils/JobletCallbackUtil.java
+++ src/main/java/com/liveramp/daemon_lib/utils/JobletCallbackUtil.java
@@ -9,25 +9,25 @@ import com.liveramp.daemon_lib.JobletConfig;
 public class JobletCallbackUtil {
 
   @SafeVarargs
-  public static <T extends JobletConfig> JobletCallback<T> compose(JobletCallback<T>... callbacks) {
+  public static <T extends JobletConfig> JobletCallback<T> compose(JobletCallback<? super T>... callbacks) {
     return new ComposeJobletCallback<>(Arrays.asList(callbacks));
   }
 
-  public static <T extends JobletConfig> JobletCallback<T> compose(List<JobletCallback<T>> callbacks) {
+  public static <T extends JobletConfig> JobletCallback<T> compose(List<JobletCallback<? super T>> callbacks) {
     return new ComposeJobletCallback<>(callbacks);
   }
 
   static class ComposeJobletCallback<T extends JobletConfig> implements JobletCallback<T> {
 
-    private final List<JobletCallback<T>> callbacks;
+    private final List<JobletCallback<? super T>> callbacks;
 
-    ComposeJobletCallback(List<JobletCallback<T>> callbacks) {
+    ComposeJobletCallback(List<JobletCallback<? super T>> callbacks) {
       this.callbacks = callbacks;
     }
 
     @Override
     public void callback(T config) throws DaemonException {
-      for (JobletCallback<T> callback : callbacks) {
+      for (JobletCallback<? super T> callback : callbacks) {
         callback.callback(config);
       }
     }
